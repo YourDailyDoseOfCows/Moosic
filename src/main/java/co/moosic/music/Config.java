85cd23cefdb6e7b261b2b6cffe3d9720a47a7173
Tyler Sparling <tylersparling@tylers-air.lan>
2020-08-26 14:14:19 -0400

    commit

--- src/main/java/co/moosic/music/Config.java
+++ src/main/java/co/moosic/music/Config.java
@@ -4,7 +4,7 @@ import com.kaaz.configuration.ConfigurationOption;
 
 public class Config {
     @ConfigurationOption
-    public static String discord_token = "your-discord-token";
+    public static String discord_token = "NzA0NDIxMTQzNDY5MzU5MTY0.Xqc5UA.uZyGQfQcj-jms6STEvwwwlT8QBU";
 
     @ConfigurationOption
     public static String command_prefix = "!";
@@ -13,5 +13,5 @@ public class Config {
     public static int volume = 30;
 
     @ConfigurationOption
-    public static String voice_channel_id = "channel-id";
+    public static String voice_channel_id = "747553716223344770";
 }
